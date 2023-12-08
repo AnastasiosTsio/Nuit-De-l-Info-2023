41fcc07263e6384406d7adfd5ca315fb16039469
Anastasios TSIOMPANIDIS <anastasios.tsiompanidis@etu.univ-grenoble-alpes.fr>
2023-12-08 02:51:58 +0100

    Moved function to the right place

--- dist/gameloop.js
+++ dist/gameloop.js
@@ -6,23 +6,18 @@ var ctx = canvas.getContext("2d");
 var cloud = new Cloud(ctx);
 var player = new Player(ctx);
 var date = new gameDate(1965, ctx);
-function clearCanvas() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-}
-
-//
 var backgroundImageIndex = 0;
-var backgroundImages = ["url('images/paysage-1.png')", "url('images/paysage-2.png')", "url('images/paysage-3.png')", "url('images/paysage-4.png')","url('images/paysage-0.png') "];
+var backgroundImages = ["url('images/paysage-1.png')", "url('images/paysage-2.png')", "url('images/paysage-3.png')", "url('images/paysage-4.png')", "url('images/paysage-0.png') "];
 var bg = document.getElementById("changebg");
-
 function cyclebg() {
     console.log("cyclebg");
     document.body.style.backgroundImage = backgroundImages[backgroundImageIndex];
     backgroundImageIndex = (backgroundImageIndex + 1) % backgroundImages.length;
 }
 bg.addEventListener("click", cyclebg);
-
-
+function clearCanvas() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
 function updateGame() {
     clearCanvas();
     date.drawDate();
@@ -33,7 +28,6 @@ function updateGame() {
 }
 var noButton = document.getElementById("noButton");
 var yesButton = document.getElementById("yesButton");
-
 noButton.addEventListener("click", cloud.changeMessage.bind(cloud));
 noButton.addEventListener("click", date.updateDate.bind(date));
 yesButton.addEventListener("click", date.updateDate.bind(date));
--- src/gameloop.ts
+++ src/gameloop.ts
@@ -8,6 +8,18 @@ let cloud = new Cloud(ctx);
 let player = new Player(ctx);
 let date = new gameDate(1965, ctx);
 
+var backgroundImageIndex = 0;
+var backgroundImages = ["url('images/paysage-1.png')", "url('images/paysage-2.png')", "url('images/paysage-3.png')", "url('images/paysage-4.png')","url('images/paysage-0.png') "];
+var bg = document.getElementById("changebg");
+
+function cyclebg() {
+    console.log("cyclebg");
+    document.body.style.backgroundImage = backgroundImages[backgroundImageIndex];
+    backgroundImageIndex = (backgroundImageIndex + 1) % backgroundImages.length;
+}
+bg.addEventListener("click", cyclebg);
+
+
 function clearCanvas() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
