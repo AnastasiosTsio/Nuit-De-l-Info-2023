69849ccd80717b3b6fa316cf972629b9942620e0
Matthieu Muller <matthieu.muller2@etu.univ-grenoble-alpes.fr>
2023-12-08 03:33:44 +0100

    commit GetConstantEffects

--- src/ConstantEffect.ts
+++ src/ConstantEffect.ts
@@ -1,34 +1,34 @@
 
 
 function GetConstantEffect() {
-    if (environment<100 || globalHealth<100 ) {
-        var i: number = 100-(environment+globalHealth);
-        if (numberOfInhabitant>i) {
-            numberOfInhabitant-=i;
+    if (environment < 100 || globalHealth < 100) {
+        var i: number = 100 - (environment + globalHealth);
+        if (numberOfInhabitant > i) {
+            numberOfInhabitant -= i;
         }
         else {
-            numberOfInhabitant=0;
+            numberOfInhabitant = 0;
         }
     }
-    else if (numberOfInhabitant>10000) {
-        frustration+=10;
-        globalMoney+=50;
-        if (environment>10) {
-            environment-=10;
+    else if (numberOfInhabitant > 10000) {
+        frustration += 10;
+        globalMoney += 50;
+        if (environment > 10) {
+            environment -= 10;
         }
         else {
-            environment=0
+            environment = 0
         }
-        
+
     }
-    else if (numberOfInhabitant<100) {
-        if (development>10) {
-            development-=10;
+    else if (numberOfInhabitant < 100) {
+        if (development > 10) {
+            development -= 10;
         }
         else {
-            development=0
+            development = 0
         }
-        
+
     }
 }
 // fonction à appeler à la fin de chaque question qui gère 
