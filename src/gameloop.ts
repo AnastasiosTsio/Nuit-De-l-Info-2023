34257d9233bc0c3dcbea2488d46b51650b64746e
noahkohrs <noah@kohrs.fr>
2023-12-08 07:30:05 +0100

    removing debug

--- src/gameloop.ts
+++ src/gameloop.ts
@@ -46,7 +46,6 @@ function clearCanvas() {
 function updateGame() {
   clearCanvas();
   date.drawDate();
-  console.log(cloud.isCloudVisible);
   expert.drawExpert();
   cloud.drawCloud();
   requestAnimationFrame(updateGame);
